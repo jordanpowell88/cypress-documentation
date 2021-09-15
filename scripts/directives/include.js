635354844ea709c087971ea3f8e5ce8de3ca96c1
Ben Alman <cowboy@rj3.net>
2021-09-15 13:46:01 -0400

    fixup! fixup! Refactor include directive

--- scripts/directives/include.js
+++ scripts/directives/include.js
@@ -1,19 +1,13 @@
 const fs = require('fs')
 const path = require('path')
 
-function msg(str) {
-  return str.replace(/\s+/g, ' ')
-}
-
 function processNode(node, { error }) {
   const { attributes } = node
   const { file } = attributes
 
   if (!file) {
     return error(
-      msg(`Found a "::include" directive without a "file" attribute. You might have intended to
-      import a partial into a markdown file, but no partial can be found without the "file"
-      attribute. The "::include" directive should look like "::include{file=path/to/file}".`)
+      `Found a "::include" directive without a "file" attribute. You might have intended to import a partial into a markdown file, but no partial can be found without the "file" attribute. The "::include" directive should look like "::include{file=path/to/file}".`
     )
   }
 
@@ -23,9 +17,7 @@ function processNode(node, { error }) {
     return fs.readFileSync(absPath, { encoding: 'utf8' })
   } catch (err) {
     return error(
-      msg(`Failed to read file: ${file}. This error is due to a problem with partials. Check your
-      markdown files for the "::include{file=PATH_TO_FILE}" directive and make sure that PATH_TO_FILE
-      exists. If this issue persists, please open a new issue with steps to reproduce.`),
+      `Failed to read file: ${file}. This error is due to a problem with partials. Check your markdown files for the "::include{file=PATH_TO_FILE}" directive and make sure that PATH_TO_FILE exists. If this issue persists, please open a new issue with steps to reproduce.`,
       err
     )
   }
